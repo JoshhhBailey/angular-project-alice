e6d4cca76f012fcf5be22e334de6abcb444c6a7e
Josh Bailey <joshhhbailey@joshs-mbp.home>
2020-05-29 11:14:10 +0100

    Home - Animation complete. Nav buttons removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Project: Alice';
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() { }
   
