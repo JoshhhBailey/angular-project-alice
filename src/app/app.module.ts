de4a5b70580f448673d3de70e8bb2a27b8f8a823
Josh Bailey <joshhhbailey@gmail.com>
2020-10-19 13:56:03 +0100

    Complete Site - v1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { WorkComponent } from './work/work.component';
 import { PlayComponent } from './play/play.component';
 import { MeComponent } from './me/me.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
 import { AllDifferentAllNormalComponent } from './project-pages/all-different-all-normal/all-different-all-normal.component';
 import { HeaderComponent } from './components/project-pages/header/header.component';
 import { BurgerMenuComponent } from './components/project-pages/burger-menu/burger-menu.component';
@@ -26,7 +25,6 @@ import { BbcRadioOneComponent } from './project-pages/bbc-radio-one/bbc-radio-on
     PlayComponent,
     MeComponent,
     FooterComponent,
-    CarouselComponent,
     AllDifferentAllNormalComponent,
     HeaderComponent,
     BurgerMenuComponent,
